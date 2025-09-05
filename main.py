4d087bb34dc2c8313f789e862f829b650d844351
anshika01-agrawal <23btc106@ietdavv.edu.in>
2025-09-05 23:57:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,10 @@ def ask(q: str):
         return {"answer": qa_pairs[q_lower]}
     else:
         return {"answer": "Sorry, I don’t have an answer for that yet."}
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+
